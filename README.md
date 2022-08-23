ab327604fa554b1baf33f69517ee81d12a7b7f6d
Martín <magadiflo@gmail.com>
2022-08-23 11:26:03 -0500

    Inicio del proyecto: JWT - Refresh Tokens

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Boot and Spring Security with JWT including Access and Refresh Tokens 🔑
+Tomado del canal de [Amigoscode, impartido por get Arrays](https://www.youtube.com/watch?v=VVn9OG9nfH0&t=1069s)
---
+++ src/main/java/com/magadiflo/app/SbSsJwtRefreshTokenApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSsJwtRefreshTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSsJwtRefreshTokenApplication.class, args);
+	}
+
+}
