0e50afbf97b71bca0e6a94345145d4afc051f0f9
Martín <magadiflo@gmail.com>
2022-08-24 12:18:19 -0500

    API Resource - Part 2

--- src/main/java/com/magadiflo/app/api/UserResource.java
+++ src/main/java/com/magadiflo/app/api/UserResource.java
@@ -1,13 +1,15 @@
 package com.magadiflo.app.api;
 
+import com.magadiflo.app.domain.Role;
 import com.magadiflo.app.domain.User;
+import com.magadiflo.app.dto.RoleToUserFormDto;
 import com.magadiflo.app.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,7 +21,25 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(this.userService.getUsers());
+        return ResponseEntity.ok().body(this.userService.getUsers());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v0/users").toUriString());
+        return ResponseEntity.created(uri).body(this.userService.saveUser(user));
+    }
+
+    @PostMapping(path = "/role")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v0/users/role").toUriString());
+        return ResponseEntity.created(uri).body(this.userService.saveRole(role));
+    }
+
+    @PostMapping(path = "/role-to-user")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserFormDto form) {
+        this.userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
     }
 
 }
---
+++ src/main/java/com/magadiflo/app/dto/RoleToUserFormDto.java
@@ -0,0 +1,11 @@
+package com.magadiflo.app.dto;
+
+import lombok.Data;
+
+@Data
+public class RoleToUserFormDto {
+
+    private String username;
+    private String roleName;
+
+}
